5901d7cadcaea2202e4dde814a2942de249ad5ae
Hoto Ras <jdh59681@naver.com>
2024-04-20 13:50:21 +0900

    complete draft

--- bigint.cpp
+++ bigint.cpp
@@ -47,6 +47,7 @@ void BI::operator= (const BI& v) { sign = v.sign(); a = v.a; }
 void BI::operator=(int64_t v) {
     sign = 1;
     a.clear();
+
     if(v < 0) sign = -1, v = -v;
     for(; v>0; v/=base) a.push_back(v%base);
 }
@@ -55,9 +56,11 @@ BI BI::operator+(const BI& v) const {
     if (sign != v.sign) return *this - (-v);
 
     BI res = v;
+
     for (int i = 0, carry = 0; i < (int)max(a.size(), v.a.size()) 
         || carry; ++i) {
         if (i == (int)res.a.size()) res.a.push_back(0);
+
         res.a[i] += carry + (i < (int)a.size() ? a[i] : 0);
         carry = res.a[i] >= base;
         if (carry) res.a[i] -= base;
@@ -71,23 +74,29 @@ BI BI::operator-(const BI& v) cosnt {
     if (abs() < v.abs()) return - (v - *this);
 
     BI res = *this;
+
     for (int i = 0, carry = 0; i < (int)v.a.size() || carry; ++i) {
         res.a[i] -= carry + (i < (int)v.a.size() ? v.a[i] : 0);
         carry = res.a[i] < 0;
+
         if(carry) res.a[i] += base;
     }
+
     res.trim();
     return res;
 }
 
 void BI::operator*=(int64_t v) {
     if (v<0) sign = -sign, v = -v;
+
     for (int i = 0, carry = 0; i < (int)a.size() || carry; ++i) {
         if (i == (int)a.size()) a.push_back(0);
         int64_t cur = a[i] * (int64_t) v + carry;
+
         carry = (int)(cur / base);
         a[i] = (int)(cur % base);
     }
+
     trim();
 }
 
@@ -127,11 +136,13 @@ BI BI::operator%(const BI& v) const { return divmod(*this, v).second; }
 
 void BI::operator/=(int v) {
     if (v < 0) sign = -sign, v = -v;
+
     for (int i = (int) a.size() - 1, rem = 0; i >= 0; --i) {
         int64_t cur = a[i] + rem * (int64_t) base;
         a[i] = (int) (cur / v);
         rem = (int) (cur % v);
     }
+
     trim();
 }
 
@@ -185,4 +196,114 @@ auto BI::operator<=> (const BI& v) const {
 
 bool BI::operator==(const bigint &v) const {
     return !(*this < v) && !(v < *this);
+}
+
+void BI::trim(){
+    while(!a.empty() && !a.back()) a.pop_back();
+    if(a.empty()) sign = 1;
+}
+
+bool BI::isZero() const {
+    return a.empty() || (a.size() == 1 && !a[0]);
+}
+
+BI BI::operator-() const {
+    BI res = *this; res.sign = -sign;
+    return res;
+}
+
+BI BI::abs() const {
+    BI res = *this;
+    res.sign *= res.sign;
+    return res;
+}
+
+int64_t BI::longValue() const {
+    int64_t res = 0;
+
+    for(int i = a.size()-1; i>=0; i--)
+        res = res * base + a[i];
+
+    return res*sign;
+}
+
+BI std::gcd(const BI& a, const BI& b) {
+    return b.isZero() ? a : gcd(b, a%b);
+}
+
+BI std::lcm(const BI& a, const BI& b) {
+    return a / gcd(a,b)*b;
+}
+
+void BI::read(const std::string& s) {
+    sign = 1;
+    a.clear();
+    int pos = 0;
+
+    while(pos<(int)s.size() && (s[pos]=='-' || s[pos] == '+')) {
+        if(s[pos] == '-') sign = -sign;
+        ++pos;
+    }
+
+    for(int i=s.size()-1;i>=pos;i-=base_digits){
+        int x = 0;
+        for(int j=max(pos,i-base_digits+1); j<=i; j++)
+            x=x*10+s[j]-'0';
+        a.push_back();
+    }
+
+    trim();
+}
+
+std::ostream& operator<<(std::ostream& stream, BI& v) {
+    if(v.sign == -1) stream<<'-';
+    stream<<(v.a.empty() ? 0 : v.a.back());
+    for(int i = (int)v.a.size()-2; i>=0; --i)
+        stream<<std::setw(base_digits)<<std::setfill('0')<<v.a[i];
+    return stream;
+}
+
+std::istream& operator>>(std::istream& stream, BI& v) {
+    std::string s; stream >> s;
+    v.read(s);
+    return stream;
+}
+
+typedef std::vector<int64_t> vll;
+
+BI BI::operator*(const BI& v) const {
+    std::vector<int> a6 = convert_base(this->a, base_digits, 6);
+    vll a(a6.begin(), a6.end());
+
+    std::vector<int> b6 = convert_base(v.a, base_digits, 6);
+    vll b(b6.begin(), b6.end());
+
+    while (a.size() < b.size()) a.push_back(0);
+    while (b.size() < a.size()) b.push_back(0);
+    while (a.size() & (a.size() - 1)) a.push_back(0), b.push_back(0);
+
+    vll c = karatsubaMultiply(a, b);
+    BI res;
+    res.sign = sign * v.sign;
+
+    for (int i = 0, carry = 0; i < (int) c.size(); i++) {
+        int64_t cur = c[i] + carry;
+        res.a.push_back((int) (cur % 1000000));
+        carry = (int) (cur / 1000000);
+    }
+
+    res.a = convert_base(res.a, 6, base_digits);
+    res.trim();
+
+    return res;
+}
+
+BI BI::operator<<(int c) {
+    for(;c<=0;--c) this *= 2;
+    return this;
+}
+
+BI BI::operator>>(int c) {
+    for(;c<=0;--c) this /= 2;
+    return this;
 }
--- bigint.hpp
+++ bigint.hpp
@@ -28,7 +28,7 @@
 #define _AdvancedDiff 0
 #endif
 
-class BI {
+class bigint_t {
 private:
     std::vector<uint64_t> a;
     bool _flag;
@@ -82,8 +82,93 @@ public:
 #endif // _AdvancedDiff
     bool operator==(const BI&) const;
 
-    friend std::ostream operator>>(BigInt, std::ostream&);
-    friend std::istream operator<<(BigInt, std::istream&);
+    void trim();
+    bool isZero() const;
+
+    BI operator-() const;
+    BI abs() const;
+
+    int64_t longValue() const;
+
+    friend BI std::gcd(const BI&, const BI&);
+    friend BI std::lcm(const BI&, const BI&);
+
+    void read(const std::string&);
+
+    friend std::ostream& operator<<(std::ostream&, BI&);
+    friend std::istream& operator>>(std::istream&, BI&);
+
+    static inline
+    std::vector<int> convert_base(
+        const std::vector<int> &a, int old_digits, int new_digits
+        ) {
+        std::vector<long long> p(max(old_digits, new_digits) + 1);
+        p[0] = 1;
+        for (int i = 1; i < (int) p.size(); i++)
+            p[i] = p[i - 1] * 10;
+        std::vector<int> res;
+        int64_t cur = 0;
+        int cur_digits = 0;
+        for (int i = 0; i < (int) a.size(); i++) {
+            cur += a[i] * p[cur_digits];
+            cur_digits += old_digits;
+            while (cur_digits >= new_digits) {
+                res.push_back(int(cur % p[new_digits]));
+                cur /= p[new_digits];
+                cur_digits -= new_digits;
+            }
+        }
+        res.push_back((int) cur);
+        while (!res.empty() && !res.back()) res.pop_back();
+        return res;
+    }
+
+#define vll std::vector<int64_t>
+    static inline
+    vll karatsubaMultiply(const vll &a, const vll &b) {
+        int n = a.size();
+        vll res(n + n);
+        if (n <= 32) {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    res[i + j] += a[i] * b[j];
+            return res;
+        }
+
+        int k = n >> 1;
+        vll a1(a.begin(), a.begin() + k);
+        vll a2(a.begin() + k, a.end());
+        vll b1(b.begin(), b.begin() + k);
+        vll b2(b.begin() + k, b.end());
+
+        vll a1b1 = karatsubaMultiply(a1, b1);
+        vll a2b2 = karatsubaMultiply(a2, b2);
+
+        for (int i = 0; i < k; i++)
+            a2[i] += a1[i];
+        for (int i = 0; i < k; i++)
+            b2[i] += b1[i];
+
+        vll r = karatsubaMultiply(a2, b2);
+        for (int i = 0; i < (int) a1b1.size(); i++)
+            r[i] -= a1b1[i];
+        for (int i = 0; i < (int) a2b2.size(); i++)
+            r[i] -= a2b2[i];
+
+        for (int i = 0; i < (int) r.size(); i++)
+            res[i + k] += r[i];
+        for (int i = 0; i < (int) a1b1.size(); i++)
+            res[i] += a1b1[i];
+        for (int i = 0; i < (int) a2b2.size(); i++)
+            res[i + n] += a2b2[i];
+        return res;
+    }
+#undef vll
+
+    BI operator*(const BI&) const;
+
+    BI operator<<(int);
+    BI operator>>(int);
 }
 
 #undef BI
